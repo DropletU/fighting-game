74524f95650371150f76287a735bba79599263bc
DropletU <dropletudi@gmail.com>
2026-05-04 10:35:11 +0300

    Update InputActions autoload to have the information it needs

--- Global Scripts/input_actions.gd
+++ Global Scripts/input_actions.gd
@@ -1,12 +1,14 @@
 extends Node
 
+# The input priority is "updownleftrightonetwothreefourmagic_button"
+
 ## Returns [code]true[/code] if there is an action that can be or has been reached
 ## from [member frame_being_checked].
-func check_action_validity(frame_being_checked):
+func check_action_validity(_action, _frame_being_checked):
 	return true
 	
 
 ## Returns the most suitable action it can find from [member action]
-func get_most_suitable_valid_action(action):
+func get_most_suitable_valid_action(action: Array):
 	if action.size()==0: return
 	return ["placeholder action"]
