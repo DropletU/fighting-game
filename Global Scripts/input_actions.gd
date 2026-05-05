b58d29b669a2768d144a49ba8ed30e8c8a6fdf67
DropletU <dropletudi@gmail.com>
2026-05-05 09:28:36 +0300

    Add reminders in InputActions autoload

--- Global Scripts/input_actions.gd
+++ Global Scripts/input_actions.gd
@@ -1,6 +1,6 @@
 extends Node
 
-# The input priority is "updownleftrightonetwothreefourmagic_button"
+# The input priority is one two three four magic_button"
 
 ## Returns [code]true[/code] if there is an action that can be or has been reached
 ## from [member frame_being_checked].
@@ -11,4 +11,7 @@ func check_action_validity(_action, _frame_being_checked):
 ## Returns the most suitable action it can find from [member action]
 func get_most_suitable_valid_action(action: Array):
 	if action.size()==0: return
-	return ["placeholder action"]
+	return [{"placeholder action": "placeholder action"}]
+	# This should return which action will be made, which state should be entered
+	# next, and how many frames should pass until the next state starts. 0 should
+	# mean start the next state immediately.
