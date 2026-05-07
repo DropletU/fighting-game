37a0b6caa32fa3e5b591cc073e310cfcff2f73ca
DropletU <dropletudi@gmail.com>
2026-05-07 13:37:02 +0300

    Complete input_display's code and add it to player scene

---
+++ Scripts/input_display.gd
@@ -0,0 +1,70 @@
+extends Control
+
+@onready var up = $Up
+@onready var down = $Down
+@onready var left = $Left
+@onready var right = $Right
+@onready var one = $One
+@onready var two = $Two
+@onready var three = $Three
+@onready var four = $Four
+@onready var magic = $RightShift
+
+func _ready() -> void:
+	$ColorRect.size=self.size
+	
+
+
+func _physics_process(delta: float) -> void:
+	pass
+	
+	
+
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("up"):
+		up.button_pressed=true
+	elif event.is_action_released("up"):
+		up.button_pressed=false
+	
+	if event.is_action_pressed("down"):
+		down.button_pressed=true
+	elif event.is_action_released("down"):
+		down.button_pressed=false
+	
+	if event.is_action_pressed("left"):
+		left.button_pressed=true
+	elif event.is_action_released("left"):
+		left.button_pressed=false
+	
+	if event.is_action_pressed("right"):
+		right.button_pressed=true
+	elif event.is_action_released("right"):
+		right.button_pressed=false
+	
+	if event.is_action_pressed("one"):
+		one.button_pressed=true
+	elif event.is_action_released("one"):
+		one.button_pressed=false
+	
+	if event.is_action_pressed("two"):
+		two.button_pressed=true
+	elif event.is_action_released("two"):
+		two.button_pressed=false
+	
+	if event.is_action_pressed("three"):
+		three.button_pressed=true
+	elif event.is_action_released("three"):
+		three.button_pressed=false
+	
+	if event.is_action_pressed("four"):
+		four.button_pressed=true
+	elif event.is_action_released("four"):
+		four.button_pressed=false
+	
+	if event.is_action_pressed("magic_button"):
+		magic.button_pressed=true
+	elif event.is_action_released("magic_button"):
+		magic.button_pressed=false
+	
