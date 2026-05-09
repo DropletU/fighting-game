167efe8bbac45c3e598a9bec044c2286b906aac2
DropletU <dropletudi@gmail.com>
2026-05-09 18:54:48 +0300

    Add input display node to player node
    It currently displays the frame count of the current input, but not the actual current input. It can display up to 24, which will probably be lowered when the key display is added

--- Scripts/Input Scripts/input_history_display.gd
+++ Scripts/Input Scripts/input_history_display.gd
@@ -1,8 +1,51 @@
 extends Node
 
 @onready var history_node = $"../InputHistory"
+@onready var vbox = $VBoxContainer
+var frame_data: Array
+
+
+func _ready() -> void:
+	pass
+	
+
+
+func add_display_slot():
+	var slot = HBoxContainer.new()
+	
+	# Add icon slots
+	for j in 5:
+		var icon = TextureRect.new()
+		icon.visible=false
+		slot.add_child(icon)
+		var texture = AtlasTexture.new()
+		texture.atlas = load("res://Assets/icons-keyboard-16x16-1bit-ansdor.png")
+		texture.region = Rect2(0, 0, 16, 16)
+		icon.texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
+		icon.texture = texture
+	
+	# Add counter slot
+	var counter = Label.new()
+	
+	counter.text = str(1)
+	slot.add_child(counter)
+	
+	vbox.add_child(slot)
+	vbox.move_child(slot, 0)
+	if vbox.get_child_count()>24:
+		vbox.remove_child(vbox.get_child(-1))
+	
 
 
 func _physics_process(_delta: float) -> void:
-	var new_frame_data=history_node.input_history
+	if vbox.get_child_count()==0:
+		add_display_slot()
+	var newest_frame=history_node.input_history.back()
+	if frame_data==newest_frame:
+		var counter = vbox.get_child(0).get_child(-1) # Text label holding the frame count
+		counter.text = str(int(counter.text)+1)
+		return
+	frame_data=history_node.input_history.back()
+	add_display_slot()
+	
 	
