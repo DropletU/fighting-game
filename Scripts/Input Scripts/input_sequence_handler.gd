3d72cc10adadb8fcae3d44ce5549b16639993775
DropletU <dropletudi@gmail.com>
2026-05-10 19:29:19 +0300

    Finish the InputSequenceHandler Node and script
    PLEASE TELL ME THERES NO BUGS THIS TIME PLELLEASESEEEEE

--- Scripts/Input Scripts/input_sequence_handler.gd
+++ Scripts/Input Scripts/input_sequence_handler.gd
@@ -5,7 +5,7 @@ var current_sequence: Array[String] = []
 @export var sequence_buffer:=15
 var buffer_timer:=0
 var sequence_started:=false
-var skip_action_frame: = false
+var waiting_for_release: = false
 var last_input:="neutral"
 
 @onready var input_history = $"../InputHistory"
@@ -27,15 +27,17 @@ func _physics_process(_delta: float) -> void:
 	var second_last_frame: Array = history[-2]
 	var current_input:=""
 	current_input = track_movement_keys(second_last_frame, current_input)
-	current_input = track_action_keys(history, second_last_frame, current_input)
+	current_input = track_action_keys(history.back(), second_last_frame, current_input)
 	
 	if current_input!=last_input:
 		last_input=current_input
 		if match_from_move_list(current_input):
-			print("matched")
 			sequence_started=true
 			buffer_timer=0
 			current_sequence.append(current_input)
+			print("current sequence ", current_sequence)
+		else:
+			buffer_timer+=sequence_buffer
 	
 	if sequence_started:
 		buffer_timer+=1
@@ -55,41 +57,51 @@ func track_movement_keys(second_last_frame: Array, current_input: String):
 		current_input+="neutral"
 		return current_input
 	
-	for input in second_last_frame:
-		if input=="up" or input=="down":
-			current_input+=input
-		elif input=="right":
-			current_input+="forward" if facing=="right" else "back"
-		elif input=="left":
-			current_input+="forward" if facing=="left" else "back"
+	current_input = parse_movement_keys(second_last_frame, facing)
 	return current_input
 	
 
-func track_action_keys(history: Array, second_last_frame: Array, current_input: String):
-	if skip_action_frame:
-		skip_action_frame=false
-		return current_input
+
+
+func track_action_keys(last_frame: Array, second_last_frame: Array, current_input: String):
 	var action_keys = ["one", "two", "three", "four", "(R)"]
+	if waiting_for_release and second_last_frame.any(func(input): return input in action_keys):
+		return current_input
+	else: waiting_for_release=false
 	
 	if second_last_frame.all(func(i): return i not in action_keys): # No action was made
 		return current_input
 	
-	if history.back().size()<=second_last_frame.size(): # Last frame is not a superset of current
+	if last_frame.size()<=second_last_frame.size(): # Last frame is not a superset of current
 		for input in second_last_frame:
 			if input in action_keys:
 				if current_input=="neutral":
 					current_input=""
 				current_input+=input
-	elif second_last_frame.all(func(i): return i in history.back()): # Last frame is a superset
-		for input in history.back():
+	elif second_last_frame.all(func(i): return i in last_frame): # Last frame is a superset
+		print(second_last_frame, last_frame)
+		for input in last_frame:
 			if input in action_keys:
-				skip_action_frame=true
 				if current_input=="neutral":
-					current_input=""
+					current_input=parse_movement_keys(last_frame, player.facing)
 				current_input+=input
+				print(current_input)
+	waiting_for_release=true
 	return current_input
 	
 
+
+func parse_movement_keys(frame_data: Array, facing: String):
+	var parsed_input:=""
+	for input in frame_data:
+		if input=="up" or input=="down":
+			parsed_input+=input
+		elif input=="right":
+			parsed_input+="forward" if facing=="right" else "back"
+		elif input=="left":
+			parsed_input+="forward" if facing=="left" else "back"
+	return parsed_input
+	
 
 func match_from_move_list(current_input):
 	var test_sequence = current_sequence.duplicate(true)
@@ -104,7 +116,6 @@ func match_from_move_list(current_input):
 		if match_sequences(test_sequence, sequence):
 			print(stance_moves[i])
 			return true
-		continue
 	return false
 	
 
@@ -115,5 +126,7 @@ func match_sequences(test_sequence: Array, match_sequence: Array):
 		if test_sequence[i]==match_sequence[i]:
 			continue
 		return false
+	if test_sequence.size()==match_sequence.size():
+		print("Action Successful")
 	return true
 	
