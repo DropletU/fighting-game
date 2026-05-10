6a33fb413dae7868c824b9a0c384806b2fc36b0d
DropletU <dropletudi@gmail.com>
2026-05-10 15:44:19 +0300

    Move current_input and second_last_frame to _physics_process
    This is talking about the InputSequenceHandler node's script

--- Scripts/Input Scripts/input_sequence_handler.gd
+++ Scripts/Input Scripts/input_sequence_handler.gd
@@ -15,23 +15,24 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
 	var history: Array = input_history.input_history
-	track_movement_keys(history)
-	track_action_keys(history)
-	
-
-func track_action_keys(history: Array):
-	if skip_action_frame:
-		skip_action_frame=false
-		return
 	var frame_count = history.size()
-	if frame_count<=2: # Edge case of the second frame from when the player is instantiated
+	if frame_count<2: # Edge case on the first frame from when the player is instantiated
 		return
 	var second_last_frame: Array = history[-2]
-	var action_keys = ["one", "two", "three", "four", "magic_button"]
 	var current_input:=""
+	current_input = track_movement_keys(history, second_last_frame, current_input)
+	current_input = track_action_keys(history, second_last_frame, current_input)
+	
+
+func track_action_keys(history: Array, second_last_frame: Array, current_input: String):
+	if skip_action_frame:
+		skip_action_frame=false
+		return current_input
+	var action_keys = ["one", "two", "three", "four", "magic_button"]
 	
 	if second_last_frame.all(func(i): return i not in action_keys): # No action was made
-		return
+		return current_input
+	
 	if history.back().size()<=second_last_frame.size(): # Last frame is not a superset of current
 		for input in second_last_frame:
 			if input in action_keys:
@@ -41,5 +42,6 @@ func track_action_keys(history: Array):
 			if input in action_keys:
 				skip_action_frame=true
 				current_input+=input
-	current_sequence[-1]+=current_input
+	return current_input
+	
 	
