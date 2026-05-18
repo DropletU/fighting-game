92f115c2fde5cb6217aedae4ab2a338cd3390408
DropletU <dropletudi@gmail.com>
2026-05-18 06:44:28 +0300

    Connect SequenceBuilder signals to Executor

--- Scripts/Move Scripts/executor.gd
+++ Scripts/Move Scripts/executor.gd
@@ -1,3 +1,11 @@
 extends Node
 
 var executing:=false
+
+
+func _on_sequence_builder_stance_matched(sequence: Array, stance: String = "standing") -> void:
+	pass # Replace with function body.
+
+
+func _on_sequence_builder_valid_action(sequence: Array, stance: String) -> void:
+	pass # Replace with function body.
