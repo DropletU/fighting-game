d120730e846ea0e8df5a4975ede995bab90066ce
DropletU <dropletudi@gmail.com>
2026-05-28 06:49:34 +0300

    Add scene_transition() to Transitioner autoload

--- Globals/scene_transitioner.gd
+++ Globals/scene_transitioner.gd
@@ -1,11 +1,12 @@
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func scene_transition(target_scene: String, coordinates: Vector2):
+	var current_scene = get_tree().current_scene
+	var player: CharacterBody2D = get_tree().get_first_node_in_group("Player")
+	if target_scene!=current_scene.scene_file_path:
+		var new_scene = load(target_scene)
+		current_scene.queue_free()
+		get_tree().root.add_child(new_scene)
+	player.global_position=coordinates
+	
