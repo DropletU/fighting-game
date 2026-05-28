0a0dfe4af45749038001083d19020c12a48e3fed
DropletU <dropletudi@gmail.com>
2026-05-28 09:15:36 +0300

    Add getter function for the player

--- Globals/game_manager.gd
+++ Globals/game_manager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var player: CharacterBody2D
+var player: CharacterBody2D: set = set_player, get = get_player 
 var respawn_point:=Vector2.ZERO
 var player_node = preload("res://Scenes/Prefabs/Player.tscn")
 
@@ -8,7 +8,6 @@ func player_died():
 	player.visible=false
 	player.queue_free()
 	await get_tree().process_frame
-	spawn_new_player()
 	
 
 func spawn_new_player(spawn_location:=respawn_point):
@@ -20,6 +19,10 @@ func spawn_new_player(spawn_location:=respawn_point):
 
 func _handle_new_player_instance_info(instance, spawn_location: Vector2):
 	instance.global_position = spawn_location
+	
+
+func get_player():
+	return player
 	
 
 func set_player(new_player: CharacterBody2D):
