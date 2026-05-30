0bc3d7137fd7cd80c57ff5f11a608e7be917eac3
DropletU <dropletudi@gmail.com>
2026-05-30 10:36:03 +0300

    Main Menu: Add play page to main menu
    Load button was changed to play button

--- Scripts/Menu Scripts/main_menu.gd
+++ Scripts/Menu Scripts/main_menu.gd
@@ -3,39 +3,19 @@ extends Control
 @onready var details = $Details
 @onready var options_page = $Details/OptionsPage
 @onready var credits_page = $Details/CreditsPage
-@onready var load_page = $Details/LoadPage
-
-func _ready() -> void:
-	if SaveManager.save_index.has_section_key("meta", "last_used"):
-		$MainButtons/MarginContainer/VBoxContainer/Play.disabled=false
-	
+@onready var play_page = $Details/PlayPage
 
 func _on_play_pressed() -> void:
-	var coords: = Vector2(143, -48)
-	# TODO: Add a save manager and connect [member coords] to it.
-	GameManager.respawn_scene="res://Scenes/scene_one.tscn"
-	GameManager.respawn_point=coords
-	GameManager.enter_new_scene("res://Scenes/scene_one.tscn", true, coords)
-	
-
-func _on_quit_pressed() -> void:
-	get_tree().quit()
-	
-
-func disable_all_pages(disable_main:=false):
-	for page in details.get_children():
-		page.visible=false
-	if disable_main:
-		details.visible=false
-	
-
-func _on_load_pressed() -> void:
-	if load_page.visible:
+	if play_page.visible:
 		disable_all_pages(true)
 	else:
 		details.visible=true
 		disable_all_pages()
-		load_page.visible=true
+		play_page.visible=true
+	
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
 	
 
 func _on_options_pressed() -> void:
@@ -45,6 +25,7 @@ func _on_options_pressed() -> void:
 		details.visible=true
 		disable_all_pages()
 		options_page.visible=true
+	
 
 func _on_credits_pressed() -> void:
 	if credits_page.visible:
@@ -53,3 +34,15 @@ func _on_credits_pressed() -> void:
 		details.visible=true
 		disable_all_pages()
 		credits_page.visible=true
+	
+
+func disable_all_pages(disable_main:=false):
+	for page in details.get_children():
+		page.visible=false
+	if disable_main:
+		details.visible=false
+	
+
+
+func _on_save_one_pressed() -> void:
+	pass
