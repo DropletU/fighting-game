a93e0babca58ad2df23a7e94c84d8c15f5d1a48e
DropletU <dropletudi@gmail.com>
2026-05-30 10:22:36 +0300

    Main Menu: Add load button and optimize code

--- Scripts/Menu Scripts/main_menu.gd
+++ Scripts/Menu Scripts/main_menu.gd
@@ -1,35 +1,55 @@
 extends Control
 
 @onready var details = $Details
-@onready var options = $Details/OptionsDetails
-@onready var credits = $Details/CreditsDetails
+@onready var options_page = $Details/OptionsPage
+@onready var credits_page = $Details/CreditsPage
+@onready var load_page = $Details/LoadPage
+
+func _ready() -> void:
+	if SaveManager.save_index.has_section_key("meta", "last_used"):
+		$MainButtons/MarginContainer/VBoxContainer/Play.disabled=false
+	
 
 func _on_play_pressed() -> void:
 	var coords: = Vector2(143, -48)
 	# TODO: Add a save manager and connect [member coords] to it.
-	GameManager.enter_new_scene("res://Scenes/scene_one.tscn", true, coords)
 	GameManager.respawn_scene="res://Scenes/scene_one.tscn"
 	GameManager.respawn_point=coords
+	GameManager.enter_new_scene("res://Scenes/scene_one.tscn", true, coords)
 	
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 	
 
-func _on_options_pressed() -> void:
-	if options.visible:
+func disable_all_pages(disable_main:=false):
+	for page in details.get_children():
+		page.visible=false
+	if disable_main:
 		details.visible=false
-		options.visible=false
+	
+
+func _on_load_pressed() -> void:
+	if load_page.visible:
+		disable_all_pages(true)
 	else:
 		details.visible=true
-		options.visible=true
-		credits.visible=false
+		disable_all_pages()
+		load_page.visible=true
+	
+
+func _on_options_pressed() -> void:
+	if options_page.visible:
+		disable_all_pages(true)
+	else:
+		details.visible=true
+		disable_all_pages()
+		options_page.visible=true
 
 func _on_credits_pressed() -> void:
-	if credits.visible:
-		details.visible=false
-		credits.visible=false
+	if credits_page.visible:
+		disable_all_pages(true)
 	else:
 		details.visible=true
-		credits.visible=true
-		options.visible=false
+		disable_all_pages()
+		credits_page.visible=true
