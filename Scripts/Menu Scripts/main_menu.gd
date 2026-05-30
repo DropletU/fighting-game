3973886a6fea6ef9334126bc2f181f5efc30dfcf
DropletU <dropletudi@gmail.com>
2026-05-30 12:01:15 +0300

    Main Menu: Remove Details packed scene and code

--- Scripts/Menu Scripts/main_menu.gd
+++ Scripts/Menu Scripts/main_menu.gd
@@ -1,6 +1,5 @@
 extends Control
 
-@onready var details = $Details
 
 func _on_play_pressed() -> void:
 	GameManager.enter_new_scene("res://Scenes/scene_one.tscn", true, Vector2(143, -48))
@@ -9,15 +8,6 @@ func _on_play_pressed() -> void:
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 	
-
-
-func disable_all_pages(disable_main:=false):
-	for page in details.get_children():
-		page.visible=false
-	if disable_main:
-		details.visible=false
-	
-
 
 func _on_save_one_pressed() -> void:
 	pass
