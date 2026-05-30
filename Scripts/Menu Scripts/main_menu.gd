44082d4439ad429de1818d368f87ec7bed5dd5a6
DropletU <dropletudi@gmail.com>
2026-05-30 11:56:33 +0300

    Main Menu: Remove Details page

--- Scripts/Menu Scripts/main_menu.gd
+++ Scripts/Menu Scripts/main_menu.gd
@@ -1,40 +1,15 @@
 extends Control
 
 @onready var details = $Details
-@onready var options_page = $Details/OptionsPage
-@onready var credits_page = $Details/CreditsPage
-@onready var play_page = $Details/PlayPage
 
 func _on_play_pressed() -> void:
-	if play_page.visible:
-		disable_all_pages(true)
-	else:
-		details.visible=true
-		disable_all_pages()
-		play_page.visible=true
+	GameManager.enter_new_scene("res://Scenes/scene_one.tscn", true, Vector2(143, -48))
 	
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 	
 
-func _on_options_pressed() -> void:
-	if options_page.visible:
-		disable_all_pages(true)
-	else:
-		details.visible=true
-		disable_all_pages()
-		options_page.visible=true
-	
-
-func _on_credits_pressed() -> void:
-	if credits_page.visible:
-		disable_all_pages(true)
-	else:
-		details.visible=true
-		disable_all_pages()
-		credits_page.visible=true
-	
 
 func disable_all_pages(disable_main:=false):
 	for page in details.get_children():
