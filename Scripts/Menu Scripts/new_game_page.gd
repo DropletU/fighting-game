81a0a62b280899d4ece66b83e2794af689068a1e
DropletU <dropletudi@gmail.com>
2026-05-31 17:13:25 +0300

    Main Menu: Fix bug where player could start game without setting a name

--- Scripts/Menu Scripts/new_game_page.gd
+++ Scripts/Menu Scripts/new_game_page.gd
@@ -9,15 +9,20 @@ func _ready() -> void:
 		difficulties[i].button_info = difficulties[i].name.to_lower()
 	
 
+func start_new_game(info):
+	if text=="":
+		return
+	GameManager.start_new_game(text, info)
+	
 
 func _on_normal_button_pressed(info: String) -> void:
-	GameManager.start_new_game(text, info)
+	start_new_game(info)
 
 func _on_deathless_button_pressed(info: String) -> void:
-	GameManager.start_new_game(text, info)
+	start_new_game(info)
 
 func _on_hitless_button_pressed(info: String) -> void:
-	GameManager.start_new_game(text, info)
+	start_new_game(info)
 
 
 func _on_name_edit_text_changed(new_text: String) -> void:
