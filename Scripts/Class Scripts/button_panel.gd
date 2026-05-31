7193fc1fe53e8326430e4ca29f41cb65b95b9a72
DropletU <dropletudi@gmail.com>
2026-05-31 17:12:45 +0300

    Main Menu: Add and connect PanelButton class to Main Menu play page

--- Scripts/Class Scripts/button_panel.gd
+++ Scripts/Class Scripts/button_panel.gd
@@ -1,11 +1,18 @@
 extends PanelContainer
 
+class_name ButtonPanel
+
 var hovering:=false
 var button_info:=""
-@onready var name_node = find_child("Name")
 
 ## Emits when the panel is presed while being hovered over.
 signal button_pressed(info: String)
+
+func _ready() -> void:
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+	gui_input.connect(_on_gui_input)
+	
 
 func _on_mouse_entered() -> void:
 	hovering=true
---
+++ Scripts/Menu Scripts/save_slot_button.gd
@@ -0,0 +1,5 @@
+extends ButtonPanel
+
+@onready var name_node = $MarginContainer/Name
+@onready var time_played_node = $MarginContainer/TimePlayed
+@onready var last_location_node = $MarginContainer/LastLocation
