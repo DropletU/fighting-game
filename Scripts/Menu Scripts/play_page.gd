3fee8d27c0237b125d25197840dc0aa08331a90e
DropletU <dropletudi@gmail.com>
2026-05-31 14:57:19 +0300

    Main Menu: Connect new game slots and signals to play page

---
+++ Scripts/Menu Scripts/play_page.gd
@@ -0,0 +1,17 @@
+extends PanelContainer
+
+
+func _on_button_one_start_new_save() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_two_start_new_save() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_three_start_new_save() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_four_start_new_save() -> void:
+	pass # Replace with function body.
